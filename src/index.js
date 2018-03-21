56700afd9d8b554d23cfabe42771b05a83c77f64
Yacine Hmito <yacine.hmito@gmail.com>
2018-03-21 21:42:57 +0100

    Setup firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+  initializeApp as initFirebaseApp,
+  firestore as initFirestore,
+} from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+};
+
+initFirebaseApp(firebaseConfig);
+
+const db = initFirestore();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
